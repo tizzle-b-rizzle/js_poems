2c10a477d7c8ae16c9ba8bf5b77c1e2fc1dffbfb
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-07-25 20:19:59 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var listOfLukesPoems = ["a", "b", "c", "d"] //List full of Luke's poems
 
 var randomNumber = Math.floor(Math.random() * (listOfLukesPoems.length)); //generates a random number between 0 and the length of the list of the poems
 
-function test() {
+function test() { //function changes the text in the html div "poem" to a random item out of the listOfLukesPoems array
     var poem = document.getElementById('poem');
     poem.innerHTML = listOfLukesPoems[randomNumber]
 }
