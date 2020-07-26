b76956437090ea75ec7d1ece684b981e0a6b83a9
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-07-26 13:08:27 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,568 @@
-var listOfLukesPoems = ["a", "b", "c", "d"] //List full of Luke's poems
+//#region the actual poems
+var poemStorm = `
+I sense a storm brewing.
+Atop the bank looking down,
+Into the quarry
+a dirty scar.
+
+The acrid wall of cloud closes in,
+Stealing my shadow away.
+It matches my mood.
+
+My cigarette is good for one last drag,
+The ember spits back as it hits the floor,
+I quit. But my nerves didn’t.
+
+I need the cleanse,
+My tie whips out behind me,
+It is nearly here.
+
+A distant rumble fills the stone cemetery,
+Then a crack lights up. Everything.
+One drop of rain stains my shirt.
+
+The downpour will start soon.
+Past mistakes seem irrelevant now,
+Mother Nature, I feel your wrath.
+
+I am drenched, but feel nothing.
+The elements shroud me,
+A baptism from a higher power.
+
+Thunder rumbles in. louder louder
+Is this the eye?
+I think so. Yes.
+
+My time has come,
+It all lead up to this.
+I sensed a storm brewing.`
+var poemMyFriend = `
+Are you fulfilled, my friend?
+With how your life is going.
+
+Does the sun burn blue?
+ Or the moon shine green?
+
+Are you happy, my friend?
+With the relationships you have.
+
+Do birds fly rockets?
+ Or toads drive cars?
+
+Are you content, my friend?
+With your job you work so hard at.
+
+Are the mountains hollow?
+Or the volcanoes imagination?
+ 
+Are you satisfied, my friend?
+With how you are, inside and out.
+
+Do tides control themselves?
+Or salt water taste sweet?
+
+Are you proud, my friend?
+With all your achievements in life.
+
+Do clouds have dreams?
+Or the skies feel emotion?
+
+My friend,
+Are you sad?
+
+Tell me truly,
+Is the world evil?
+`
+var poemRoadTripOfLife = `
+Imagine life being a road.
+Each life has its own road,
+All different lengths,
+All with different views.
+
+What type of road is yours?
+A quaint country lane,
+Or a sprawling city motorway.
+A quiet suburban avenue,
+Or a secluded cobbled street.
+All has its own unique beauty.
+
+But really is it about the road?
+Or how we enjoy the drive,
+The pot holes and speed bumps,
+We have them all.
+
+My road goes over a bridge,
+Yours can too.
+Mine ends at the river,
+For a new journey upon its currents.
+`
+var poemHangoverHotel = `
+We arrive,
+In the early morning, stumble in, drop your change.
+Then. Crash.
+
+That’s the easy part.
+Here comes the hangover,
+Head in hands on the toilet.
+Piecing together last night,
+Throb, Throb, Throb.
+
+No water will help,
+This is a bad one,
+And here to stay.
+Your coffees gone cold,
+You shiver, you sip.
+It’s still a no go.
+
+Don’t stand up,
+You’ll just feel worse.
+Don’t think about what you drank,
+That’ll just make you sick.
+Oh wait. Too late.
+
+That bottle of wine,
+Two beers,
+Seven shots,
+And the doner kebab.
+All comes up.
+And up and up.
+
+Your five minute revive,
+Feels so good,
+But you’ll be back in Hangover City soon.
+Feeling worse,
+All groggy and sore.
+
+Why do you drink?
+The question rattling around your mind,
+Every time you visit,
+You know how bad it feels.
+Yet forget the ache,
+The hollowness in your head,
+Your body made of lead.
+
+No worries, 
+Next week, you’ll forget,
+Because we all have a room reserved,
+In the Hangover Hotel.
+`
+var poemBurningUnkown = `
+It’s burning,
+Over the roofs,
+Over their skies.
+An intense fire,
+And you’re the only one who sees.
+
+Down it drops,
+Plummeting to the empty ground.
+It’s been hot,
+The fields will catch.
+
+But is it just fire,
+Or something more, Maybe?
+Is it extra-terrestrial,
+Or my drunken imagination.
+I cannot tell,
+Should it be clearer by morning.
+
+I go to the site,
+And find scorched burns,
+But no remains.
+Just a charred track,
+To another unconfirmed path.
+
+It leads far and across,
+To lands not quite our own.
+But then again, how can they be ours,
+If no one cherishes them.
+There is no sign,
+Of the burning phenomenon.
+
+And I am left,
+Chasing the unknown.  
+`
+var poemNeedOrWant = `
+Do you need it, or want it?
+How hard it is to define,
+You say you need what you want,
+But never want what you need. 
+
+You build yourself up with a confused rage,
+Lost in memories.
+There sweet and safe.
+You need their comfort?
+
+I want you to listen,
+To the cliché.
+There is light at the end of the tunnel,
+But it is long,
+Why do you think you’re going down alone,
+Hold out your hand.
+We’re here too.
+
+You need the closure of knowing.
+You need it to help.
+But does the knowledge change facts,
+Or what’s happened.
+Does it build new defences?
+Against the torrent of what’s gone before.
+Exactly.
+
+Replace your wants with needs.
+There the true answer lies.
+`
+var poemDoctorGoodlove = `
+Doc, listen. But don’t help.
+The line between need and want is all blurry, 
+I crave the silence of solitude,
+Gone are the days of idle chelp.
+
+A new sense of blandness, has come into life,
+The taste is meaningless,
+Eating has become another gruelling chore,
+My emotions stuck in a constant state of strife.
+
+Doc. You still there. Good.
+I don’t know why I’m telling you this,
+You’re getting paid, nodding away the hours.
+Still, I’m told conversations are better than blood.
+
+Hmm? The girl I miss you ask?
+Gone. On with a new life, leaving
+Me here, with nothing.
+But I’ll only ever show the filthy mask.
+
+What about work?
+Ha! Full of joy that one.
+Just count the hours till home, like you.
+But I am just a common clerk.
+
+I feel so ugly, Doc.
+I have no mirrors because I cannot stand to see myself,
+It’s sad but true,
+Can’t you just put me in a room and leave on the lock.
+
+Another hour been and gone,
+Yes I agree Doc,
+
+Just another bad day.
+`
+var poemDejaVu = `
+Day in, work.
+Day out, work.
+Repeat. Over and over and over.
+
+The same days, turn 
+Into the same weeks,
+Turn into the same,
+Months turn years.
+Then before you know it,
+The same life.
+
+Try spice it up by,
+Going to a pub, you always do,
+Having the same pint at the usual table,
+Before you know it. Habit and routine.
+
+How to change.
+Because when you try,
+The past hides in the future,
+Only wearing a new face.
+
+Repetition becomes boring,
+Boring leads to content.
+So I suppose that’s, that.
+
+Would you look at the time,
+6:30 again.
+Time for another day.
+The same day as yesterday.
+`
+var poemWhatIf = `
+
+Tale as old as time,
+Beauty and the beast, 
+How sweet?
+But beauty needs beauty
+And beast is left with beast
+How true.
+
+What do we have left,
+Insecurities and scars,
+It’s redundant now.
+I know I wasn’t perfect
+But, hell. I tried,
+So hard I cried. 
+
+We both have new lives,
+Yours, happiness and joy,
+Mine, sadness and sleepless nights.
+I’m fine honestly,
+I’ve stopped caring. 
+About most things.
+
+Life goes on,
+And us with it,
+We were lovers in our prime,
+Oblivious to the growing cleft,
+Take the memories to the archives,
+To bring out in solitude,
+And wonder the age old question.
+What if?
+`
+var poemJoinMe = `
+Come on in, the water’s fine.
+Have a swim, or just float for a while,
+You can watch for a little if you want.
+It’s okay.
+I’m not leaving.
+
+She’ll be here,
+You’ll want to be in before she gets here,
+So she can join you.
+Join us. Join me.
+It’s getting lonely in here.
+
+Take your time,
+It’s cold at first, but you’ll get used to it,
+We all do.
+So if we all get used to it,
+Why not join me already.
+
+You know you want to,
+I can tell by your eyes.
+How you chew your bottom lip.
+Can you feel the water yet?
+
+Live the fantasy,
+She will come in due time,
+Don’t worry. And don’t be scared.
+It’s really quite pleasant once you’re in.
+Take the plunge,
+One quick dive.
+That’s it!
+Have a running jump.
+
+Join me, boy. 
+`
+var poemSymbols = `
+How quickly a world can change,
+From the ashes and brimstone,
+To clean offices and fresh forests.
+It came with a symbol.
+That I wear secret. Safe. 
+
+The symbol is frail. Growth.
+Shrouded in mystery, why did I choose this?
+You wouldn’t understand.
+To me it’s everything, 
+The will in despair. 
+
+Eagles. Symbol of freedom.
+But you are not free,
+Me?
+Neither free nor liberated,
+But my symbol does not lie.
+
+They are similar though.
+Comfort is brought by them, 
+Pledges are made by them.
+Pacts in blood. Secrets.
+My sweet symbol, save the martyr.
+`
+var poemFoolsTheLotOfYou = `
+The colour is magenta,
+Of what?
+Wait, it’s more violet now,
+Of. What?
+You tell me, colour by association,
+It doesn’t really matter.
+
+Try imagining a new colour,
+Already have, haven’t we all.
+What’s it most like?
+What is it associated with,
+Go from there,
+The background can matter.
+
+Black.
+Colour or shade,
+It absorbs all anyway,
+Just don’t get lost in technicalities,
+It makes no sense to.
+Smoking gun.
+That’s more of a grey,
+Isn’t it?
+
+Your colour,
+Bright or dark,
+Your choice. It’s ‘original’
+Remember.
+Your personality is reflected,
+Does it make sense now,
+Your colour is you,
+Can be dropped into sub-categories.
+
+What does your colour say about you,
+No. don’t tell me,
+Just fill out the form,
+Tick the box. Initial.
+Tell the therapist, room 14.
+
+Next.
+
+The colour is cyan,
+Of what?
+Wait, it’s more aqua now. 
+Of. What?
+Exactly.
+What is the point.
+`
+var poemMyMessage = `
+Will you meet me?
+At a predetermined place,
+At a predetermined time?
+In the early morning,
+When the view is hidden by grey,
+It will be raining,
+But that’s okay. 
+You don’t have to talk,
+Your presence is enough.
+
+If I leave,
+Will you come with me,
+But don’t follow,
+I don’t wish to lead.
+We are alone here,
+Our sanctuary for a few hours,
+Until morning melts into day.
+The rain stops,
+Grey fades to the light,
+I will be gone long before then,
+And you?
+
+For now we are alone,
+I am contempt with silence and glances,
+Your presence is enough,
+I will be here again,
+Will you meet me then?
+That is your decision,
+Mine already made.
+
+We find the details we do not like,
+Then focus on them,
+Don’t let them blind you,
+Let them scare you to excitement.
+Amaze me.
+`
+var poemRipple = `
+A rock drops,
+Into a lake,
+Sending ripples out,
+Towards the shore.
+The birds call,
+Another stone,
+We walk hand in hand,
+Around and around.
+
+The ripples come back,
+Lazily lapping up the sand,
+We sit down,
+And throw a few more.
+We don’t look at each other,
+Don’t speak,
+Just holding,
+That’s all that’s important.
+
+We sit for the day,
+Frozen by the sun,
+The lake doesn’t change,
+It’s been a still day.
+We dust ourselves off,
+Throw a few more rocks,
+Watch the ripples,
+Then adventure home.
+
+It’s late now,
+And dark,
+There is still no talk,
+Still no looks.
+I drop you at your house,
+And return back to mine,
+The silence has engulfed,
+Ripples passing through.
+
+You’re gone now,
+Far, far away,
+Will you throw a rock,
+And make a ripple. Again.
+Are you thinking of me,
+When the water laps back,
+Because I am of you,
+The ripple in my life.
+`
+var poemANewChapter = `
+So it begins.
+From the toppled empire,
+Grows a bud.
+Between the cracked stone,
+The green emerges,
+Curling up into the sunlight,
+A slow blossom unfolds.
+
+So it continues.
+More grow, taking over,
+What was once.
+Colour and birdcall engulfs all,
+Eroding down the old,
+Turing civilisation back to nature,
+Vines hike the tallest monument.
+
+So it thrives.
+There is no trace,
+Of the wonders that man made,
+Only nature,
+Only peace,
+The trees of colossal size,
+Will not be felled,
+For tarmac kingdoms.
+The baboons live in harmony,
+Eating only there share,
+Enjoying each passing season,
+How they were intended to be enjoyed.
+
+So it eventually ends.
+The baboons evolve,
+To creatures of greed.
+Skirmishes for territory begins,
+The rape of the land,
+For raw resources.
+The birdsong is heard no more,
+The colour drains out,
+Content for life how it was.
+A legend.
+
+So it concludes.  
+`
+
+//#endregion
+
+var listOfLukesPoems = 
+[poemStorm, 
+poemMyFriend, 
+poemRoadTripOfLife, 
+poemHangoverHotel, 
+poemBurningUnkown,
+poemNeedOrWant,
+poemDoctorGoodlove,
+poemDejaVu,
+poemWhatIf,
+poemJoinMe,
+poemSymbols,
+poemFoolsTheLotOfYou,
+poemMyMessage,
+poemRipple,
+poemANewChapter] //List full of the variables of Luke's poems
 
 var randomNumber = Math.floor(Math.random() * (listOfLukesPoems.length)); //generates a random number between 0 and the length of the list of the poems
 
@@ -6,3 +570,5 @@ function test() { //function changes the text in the html div "poem" to a random
     var poem = document.getElementById('poem');
     poem.innerHTML = listOfLukesPoems[randomNumber]
 }
+
+//stuff to ask Riz: how to get the HTML to have spaces between lines, ask if I could have stored all of the poems in a seperate js file and if that would tsill work (I'd only do this if I couldn't use VS code probs)
